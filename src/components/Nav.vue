34b14cf9de963ed889e233e3b73944b15bad8a7e
abdeleb <abdeleb98@gmail.com>
2022-10-07 10:25:39 +0200

    Nav logic

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,22 +1,30 @@
 <template>
   <header>
     <img src="../assets/logo.png" alt="logo">
-    <a href="/">
-    <img src="../assets/icons/logout-icon.png" alt="logout logo">
-    </a>
+    <img class="logout" @click="signOut" src="../assets/icons/logout-icon.png" alt="logout logo">
   </header>
 </template>
 
 <script setup>
+import { useRouter } from 'vue-router';
+import { supabase } from '../supabase';
 //constant to save a variable that will hold the use router method
-
+const router = useRouter();
 // constant to save a variable that will get the user from store with a computed function imported from vue
-
 // constant that calls user email from the useUSerStore
-
 // constant that saves the user email and cleans out the @client from the user
-
 // async function that calls the signOut method from the useUserStore and pushes the user back to the Auth view.
+const signOut = async () => {
+  try {
+    let { error } = await supabase.auth.signOut();
+    console.log("Test");
+    if (error) throw error;
+  } catch (error) {
+    alert(error.message);
+  } finally {
+    router.push("/auth/login");
+  }
+};
 </script>
 
 <style scoped>
@@ -38,7 +46,7 @@ header img {
   height: 2.6rem;
 }
 
-a img {
+.logout {
   width: 1.6rem;
   height: 1.6rem;
   margin-right: 8px;
